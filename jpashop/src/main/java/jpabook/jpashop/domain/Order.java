18bfc20242216416a786ee7909957f34764f9955
diheet <tjwlgmdl1107@naver.com>
2022-08-01 22:34:36 +0900

    nope

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+}
