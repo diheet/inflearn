1782569962fd1de127be2711558aba52c063fe22
seojihee <tjwlgmdl1107@naver.com>
2022-11-29 10:34:06 +0900

    실전예제 (연관관계 매핑 시작)

--- jpa-basic/src/main/java/com/example/jpabasic/JpaBasicApplication.java
+++ jpa-basic/src/main/java/com/example/jpabasic/JpaBasicApplication.java
@@ -1,13 +1,40 @@
 package com.example.jpabasic;
 
+import com.example.jpabasic.domain.Order;
+import com.example.jpabasic.domain.OrderItem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class JpaBasicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaBasicApplication.class, args);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
+            tx.commit();
+        }
+        catch (Exception e){
+            tx.rollback();
+        }
+        finally {
+            em.close();
+        }
     }
 
 }
--- jpa-basic/src/main/java/com/example/jpabasic/domain/Order.java
+++ jpa-basic/src/main/java/com/example/jpabasic/domain/Order.java
@@ -2,6 +2,8 @@ package com.example.jpabasic.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -11,10 +13,22 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @Column(name = "member_id")
-    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
@@ -25,14 +39,6 @@ public class Order {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
     }
 
     public LocalDateTime getOrderDate() {
@@ -49,5 +55,10 @@ public class Order {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
     }
 }
